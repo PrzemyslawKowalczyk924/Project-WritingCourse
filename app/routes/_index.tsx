2c9ec02153d88f15903ca69b1979f82eecab55d7
Komputer <kowalczyk.przemek@wp.pl>
2023-07-05 11:02:45 +0200

    Add navigation component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,19 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import _navigation  from "app/routes/_navigation";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Kurs Pisania" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="bg-black text-white" style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8"}}>
-      <h1>Welcome to Remix</h1>
+    <div style={{ fontFamily: "Borna", lineHeight: "1.8"}}>
+      <_navigation />
+      <h1 className="text-9xl font-bold">Amelia Pudzianowska</h1>
+      <h1 className="text-8xl">Kurs Pisania</h1>
       <ul>
         <li>
           <a
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mimi-pink': '#FFD6D9',
+        'magenta': '#a0025c'
+      },
+      fontFamily: {
+        borna: 'Borna, serif'
+      }
+    },
   },
   plugins: [],
 } satisfies Config
