c0ac14281c03a6c4579be78c5c82da6dc517dd09
Komputer <kowalczyk.przemek@wp.pl>
2023-08-09 11:35:31 +0200

    Prepare for Vercel publication ver.2

--- app/components/features/numberCounter.tsx
+++ app/components/features/numberCounter.tsx
@@ -1,27 +1,106 @@
-import { motion } from 'framer-motion';
+/**
+ * @framerSupportedLayoutWidth any
+ * @framerSupportedLayoutHeight any
+ */
 
-const CountUp = ({ max }) => {
-    const transition = {
-      duration: 1, // Czas trwania animacji
-      ease: 'easeOut', // Easing funkcja animacji
-    };
-  
+import {
+    animate,
+    motion,
+    useInView,
+    useMotionValue,
+    useTransform,
+} from "framer-motion"
+import { useEffect, useRef } from "react"
+import { addPropertyControls, ControlType } from "framer"
+
+type CounterProps = {
+    from: number
+    to: number
+    duration: number
+    fontFamily: string
+    fontSize: number
+    color: string
+}
+
+function AnimatedCounter({
+    from,
+    to,
+    duration,
+    fontFamily,
+    fontSize,
+    color,
+}: CounterProps) {
+    const count = useMotionValue(from)
+    const rounded = useTransform(count, (latest) => {
+        return Math.round(latest)
+    })
+    const ref = useRef(null)
+    const inView = useInView(ref)
+
+    // while in view animate the count
+    useEffect(() => {
+        if (inView) {
+            animate(count, to, { duration })
+        }
+    }, [count, inView, to, duration])
+
     return (
-      <motion.span
-        initial={{ opacity: 0, scale: 0.5 }} // Stan początkowy elementu
-        animate={{ opacity: 1, scale: 1 }} // Stan końcowy elementu po animacji
-        transition={transition}
-      >
-        <motion.span
-          initial={{ opacity: 0 }} // Stan początkowy dla cyfry
-          animate={{ opacity: 1 }} // Stan końcowy dla cyfry po animacji
-          transition={transition}
-          style={{ display: 'inline-block', width: '1.5rem', textAlign: 'center' }}
-        >
-          {max}
+        <motion.span ref={ref} style={{ fontFamily, fontSize, color }}>
+            {rounded}
         </motion.span>
-      </motion.span>
-    );
-  };
-  
-  export default CountUp;
+    )
+}
+
+AnimatedCounter.defaultProps = {
+    from: 0,
+    to: 15,
+    duration: 2.5,
+    fontFamily: "Arial",
+    fontSize: 24,
+    color: "#000000",
+}
+
+addPropertyControls(AnimatedCounter, {
+    from: {
+        type: ControlType.Number,
+        title: "From",
+        defaultValue: 0,
+        min: 0,
+        step: 1,
+    },
+    to: {
+        type: ControlType.Number,
+        title: "To",
+        defaultValue: 15,
+        min: 0,
+        step: 1,
+    },
+    duration: {
+        type: ControlType.Number,
+        title: "Duration",
+        defaultValue: 2.5,
+        min: 0,
+        step: 0.1,
+        displayStepper: true,
+    },
+    fontFamily: {
+        type: ControlType.String,
+        title: "Font Family",
+        defaultValue: "Arial",
+    },
+    fontSize: {
+        type: ControlType.Number,
+        title: "Font Size",
+        defaultValue: 24,
+        min: 12,
+        max: 72,
+        step: 1,
+    },
+    color: {
+        type: ControlType.Color,
+        title: "Color",
+        defaultValue: "#000000",
+    },
+})
+
+export default AnimatedCounter
