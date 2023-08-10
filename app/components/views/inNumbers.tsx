85f5760a362b707642b45c87e040fe0fb0563ade
Komputer <kowalczyk.przemek@wp.pl>
2023-08-10 13:05:56 +0200

    Add inView displaying counting of numers in InNumbers section

--- app/components/views/inNumbers.tsx
+++ app/components/views/inNumbers.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { useMotionValue, useTransform, animate, motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+import { useMotionValue, useTransform, animate, motion, useInView } from "framer-motion";
 
 const links = [
     { name: 'Nauka z mentorem', href: '#' },
@@ -10,23 +10,9 @@ const links = [
   
   export default function InNumbers() {
     
-   /*  const [percent, setPercent] = useState(0);
-    const [meetsNumber, setMeetsNumber] = useState(0);
-    const [hour, setHour] = useState(0);
-    const [weeks, setWeeksNumber] = useState(0);
-    const [members, setMembersNumer] = useState(0); */
-
+    const ref = useRef(null)
+    const isInView = useInView(ref)
     
-    /*  useEffect(() => {
-
-      if (percent === 100) return;
-
-      const timeout = setTimeout(() => {
-        setPercent(percent + 1);
-      }, 1 + percent * 3);
-    }, [percent]); */
-
-
     const countH1 = useMotionValue(0);
     const countH2 = useMotionValue(0);
     const countH3 = useMotionValue(0);
@@ -44,18 +30,25 @@ const links = [
     };
 
     useEffect(() => {
-      const animations = [
-        createAnimation(countH1, 5, 3),
-        createAnimation(countH2, 60, 3),
-        createAnimation(countH3, 3, 3),
-        createAnimation(countH4, 8, 5),
-        createAnimation(countH5, 100, 10),
-      ];
+      
+      if (isInView) {
+        const animations = [
+          createAnimation(countH1, 5, 3),
+          createAnimation(countH2, 60, 3),
+          createAnimation(countH3, 3, 3),
+          createAnimation(countH4, 8, 5),
+          createAnimation(countH5, 100, 10),
+        ];
+      
+        return () => {
+          if (isInView === true) {
+            animations.forEach((animation) => animation.stop());
+          }
+        };
+      }
+      
+    }, [isInView]);
 
-      return () => {
-        animations.forEach((animation) => animation.stop());
-      };
-    }, []);
 
 
      const stats = [
@@ -68,7 +61,7 @@ const links = [
 
 
     return (
-      <div className="relative isolate overflow-hidden bg-mimi-pink py-24 sm:py-32">
+      <div  className="relative isolate overflow-hidden bg-mimi-pink py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-4xl font-bold tracking-tighter text-black sm:text-6xl">„Napisz historię marzeń!” w liczbach:</h2>
@@ -76,7 +69,7 @@ const links = [
             Warsztaty prowadzone w małych grupkach pozwalają na integrację, nawiązywanie internetowych przyjaźni i wzajemnego wspierania się w twórczej drodze.
             </p>
           </div>
-          <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+          <div ref={ref} className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-black sm:grid-cols-2 md:flex lg:gap-x-10">
               {links.map((link) => (
                 <a key={link.name} href={link.href}>
@@ -88,9 +81,15 @@ const links = [
               {stats.map((stat, index) => (
                 <div key={stat.name} className={`flex flex-col-reverse items-center justify-center last:col-span-4 last:mt-4 `}>
                   <dt className="text-xl leading-7 text-gray-700">{stat.name}</dt>
-                  <motion.dd className={`text-5xl mb-3 font-bold leading-10 tracking-tight text-black ${
-                  index === 1 ? 'second-child-ofValue' : ''
-                } ${index === stats.length - 1 ? 'last-child-ofValue' : ''} `}>{stat.value}</motion.dd>
+                  {isInView ?
+                  
+                    <motion.dd className={`text-5xl mb-3 font-bold leading-10 tracking-tight text-black ${
+                      index === 1 ? 'second-child-ofValue' : ''
+                    } ${index === stats.length - 1 ? 'last-child-ofValue' : ''} `}>{stat.value}
+                    </motion.dd> 
+                    : ''
+                  }
+
                 </div>
               ))}
             </dl>
