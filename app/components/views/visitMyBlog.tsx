62464967d5e25eec973a25b94a21c2c352c235cc
Komputer <kowalczyk.przemek@wp.pl>
2023-09-21 02:36:17 +0200

    Add video

--- app/components/views/visitMyBlog.tsx
+++ app/components/views/visitMyBlog.tsx
@@ -3,11 +3,11 @@ import typeWriterMachine from '../../../public/movies/typewriter2.mp4';
 
 export default function VisitMyBlog() {
     return (
-        <section className="text-gray-600 body-font bg-mimi-pink z-30">
+        <section className="text-gray-600 body-font bg-lavenda-pink z-30 relative">
+            <video autoPlay muted loop className="object-cover object-center rounded-sm w-full h-full absolute opacity-10" src={typeWriterMachine} />
             <div className="max-w-commonLayout mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                    <img className="object-cover object-center shadow-2xl rounded-full w-96 h-96 opacity-80" alt="hero" src={doorsToBlog} />
-                   {/*  <video className="object-cover object-center rounded-full w-96 h-96 opacity-90" src={typeWriterMachine} /> */}
+                    <img className="object-cover object-center shadow-2xl rounded-full w-96 h-96 opacity-75" alt="hero" src={doorsToBlog} />
                 </div>
                 <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Puk! Puk!</h1>
