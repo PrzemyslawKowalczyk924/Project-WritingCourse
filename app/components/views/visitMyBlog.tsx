99f53d9a8350f8a5f4d4d74d71ab732eaf6dd665
Komputer <kowalczyk.przemek@wp.pl>
2024-02-18 16:08:58 +0100

    Forgot about relative

--- app/components/views/visitMyBlog.tsx
+++ app/components/views/visitMyBlog.tsx
@@ -29,7 +29,7 @@ export default function VisitMyBlog() {
                     {/* <img className="object-cover object-center shadow-2xl rounded-full w-96 h-96 opacity-75" alt="hero" src={kocha} /> */}
                     {/* <div className="font-black text-12xl opacity-100 text-cherry-pink scale-300 -rotate-12">&#7838;</div> */}
                 </div>
-                <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-end md:text-end items-center text-center">
+                <div className="relative lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-end md:text-end items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Stuk-puk!</h1>
                     <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
                     <div className="flex justify-center">
