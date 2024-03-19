cde14f0026ade6fdac24085f625caeb1bf77c6e4
Komputer <kowalczyk.przemek@wp.pl>
2024-03-19 03:26:40 +0100

    Transform style section in blog component

--- app/routes/_public.blog.$slug.tsx
+++ app/routes/_public.blog.$slug.tsx
@@ -9,7 +9,7 @@ export const loader: LoaderFunction = async ({ params }) => {
     console.log(params)
     const { slug } = params;
     console.log('title %$#', slug);
-    return await Posts.findOne({ slug: slug});
+    return await Posts.findOne({ slug: slug });
   } catch (error) {
     console.error('Błąd pobierania danych:', error);
     return { json: { error: 'Błąd pobierania danych' } };
@@ -22,42 +22,46 @@ export default function Post() {
   console.log(post);
   //console.log('data z postu', data);
 
+  const parts = [];
+  const maxLength = 500; // Maksymalna długość każdej części
+  for (let i = 0; i < post.description.length; i += maxLength) {
+    parts.push(post.description.substr(i, maxLength));
+  }
+
   return (
     <div className="bg-white py-24 sm:py-32 min-h-screen">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Abc</h2>
+        <div className="mx-auto lg:mx-0">
+          <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">Abc</h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
         </div>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-      
-            <article key={post._id} className="flex max-w-xl flex-col items-start justify-between">
-              <div className="flex items-center gap-x-4 text-xs">
-                <time dateTime={post.timeOfPublication} className="text-gray-500">
-                  {post.timeOfPublication}
-                </time>
-                <a
-                  href={"#"}
-                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                >
-                  {post.genre}
-                </a>
-              </div>
-              <div className="group relative">
-                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <a href={"#"}>
-                    <span className="absolute inset-0" />
-                    {post.title}
-                  </a>
-                </h3>
-                <a href={"#"}>
-                  <div dangerouslySetInnerHTML={{ __html: post.description }} className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"></div>
-                </a>
-              </div>
-            </article>
-        </div>
+        <article key={post._id}>
+          <article className="flex border-t border-b py-7 my-10 border-gray-200">
+            <div className="flex items-center justify-center gap-x-4 text-xl">
+              <p className="text-gray-500 font-extralight">
+                No. 000001
+              </p>
+              <p className="text-gray-500 font-extralight">
+                {post.genre}
+              </p>
+              <time dateTime={post.timeOfPublication} className="text-gray-500 font-extralight">
+                {post.timeOfPublication}
+              </time>
+            </div>
+          </article>
+          <h3 className="uppercase text-4xl mt-16 font-thin tracking-tight text-gray-900 group-hover:text-gray-600">
+            {post.title}
+          </h3>
+          <div className="mx-auto grid max-w-2xl grid-cols-2 grid-rows-3 gap-x-3 gap-y-10 pt-10 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            {parts.map((part, index) => (
+              <article key={index} className="flex max-w-xl flex-col items-start justify-between">
+                  <div dangerouslySetInnerHTML={{ __html: part }} className="mt-5 px-10 text-justify text-base leading-6 text-gray-600" />
+              </article>
+            ))}
+          </div>
+        </article>
       </div>
     </div>
   )
